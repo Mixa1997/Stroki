9cc435d100f75914e864c7eb1282c6b814fdc9a4
Mixa1997 <43315299+Mixa1997@users.noreply.github.com>
2020-12-24 18:36:29 +0200

    Add files via upload

---
+++ Stroki/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stroki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Лето - это то самое время года , когда каждому хочется отдохнуть , прибегая к любым видам деятельности и развлечений"; //Написал ,что в голову пришло , не указано как должна выглядеть строка
+            Console.WriteLine(s1);
+            Console.WriteLine(s1.Substring(0,66));
+            Console.WriteLine(s1.Substring(0, 33));
+            Console.ReadKey();
+        }
+    }
+}
